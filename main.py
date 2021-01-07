a3042702e66402e3ca9c8896fdae53260239e64a
Mārtiņš Ozoliņš <m.ozolinss@inbox.lv>
2021-01-07 08:52:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Komentārus var raskstīt
+#vairākās 
+#rindās"""
+print ("Sveika,pasaule!")
+print (2*2)
+print (2*2, 2*3, 2*4, "Pitons")
+print (f"ja saskaitīsim 5 ar 7 , iegūsim {5+7}.")
+print ("Sveika,"+"pasaule!")#var rakstīt komentārus
+print("Mārtiņš")
+print("Mārtiņš")
+print("Mārtiņš")
+print("Mārtiņš")
+print("Mārtiņš "*7)
+print (f"Lai aprēķinātu cik gadā ir sekundes ir 60 sekundes (1 minūte) x 60 minūtes (1 stunda) x 24 stundas (1 diennakts) x 365 diennaktis, iegūsim {60*60*24*365}.")
+print(1/2)
